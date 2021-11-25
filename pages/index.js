6d064956782746a0939c7d5cdaae59327320f739
Bogdan <b@bmazur.ru>
2021-11-25 18:33:21 +0300

    #Update

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,18 @@
-//let box = document.querySelector('.project__viewer');
 let popup = document.querySelector('.popup');
 let screen = document.querySelector('.screen');
 let closeButton = document.querySelector('.screen__close');
-let projects = document.getElementsByClassName('project');
+let viewers = document.getElementsByClassName('project__viewer');
 
-for(var i = 0; i < projects.length; i++) {
-  projects[i].addEventListener("click", openScreen);
+for(var i = 0; i < viewers.length; i++) {
+  viewers[i].addEventListener("click", openScreen);
 }
-
-//project.addEventListener('click', openScreen);
 closeButton.addEventListener('click', closeScreen);
 
-function openScreen(event) {
-  // popup.classList.add('popup_active');
-  // let box = event.target.querySelector('.project__viewer');
-  // console.log(event.target);
-  // console.log(event);
-  // if (event.target.classList.contains('element__like_checked')) {
-  //   event.target.classList.remove('element__like_checked');
-  // } else {
-  //   event.target.classList.add('element__like_checked');
-  // }
-  // 
-   //box.style = "margin:10px; height: 580px; width: 980px; overflow: hidden";
-   //screen.insertAdjacentElement('beforeend', box);
+function openScreen() {
+  popup.classList.add('popup_active');
 }
 
 function closeScreen() {
-  //box.style = "height: 540px; width: 640px; overflow: hidden";
-  //project.insertAdjacentElement('beforeend', box);
   popup.classList.remove('popup_active');
 }
 
@@ -38,7 +22,6 @@ function closeScreen() {
 // saveButton.addEventListener('click', saveForm);
 // popup.addEventListener('keyup', (event)=>{if (event.keyCode === 13) {saveForm()}});
 
-
 // function likeChange(event) {
 //   if (event.target.classList.contains('element__like_checked')) {
 //     event.target.classList.remove('element__like_checked');
@@ -46,8 +29,6 @@ function closeScreen() {
 //     event.target.classList.add('element__like_checked');
 //   }
 // }
-
-
 
 // function saveForm() {
 //   profileName.innerHTML = formName.value;
--- pages/viewer.js
+++ pages/viewer.js
@@ -1,44 +1,49 @@
 var myViewerDiv = document.getElementById('MyViewerDiv');
-      var myViewerDiv1 = document.getElementById('MyViewerDiv1');
-      var myViewerDiv2 = document.getElementById('MyViewerDiv2');
-      var myViewerDiv3 = document.getElementById('MyViewerDiv3');
+var myViewerDiv1 = document.getElementById('MyViewerDiv1');
+var myViewerDiv2 = document.getElementById('MyViewerDiv2');
+var myViewerDiv3 = document.getElementById('MyViewerDiv3');
 
-      var viewer = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv);
-      var viewer1 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv1);
-      var viewer2 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv2);
-      var viewer3 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv3);
+var viewer = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv);
+var viewer1 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv1);
+var viewer2 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv2);
+var viewer3 = new Autodesk.Viewing.Private.GuiViewer3D(myViewerDiv3);
 
-      var options = {
-          'env' : 'Local',
-          'document' : './shaver/0.svf'
-      };
+var options = {
+    'env' : 'Local',
+    'document' : './svfs/test2/Resource/3D вид/3D/3D.svf'
+};
+var options1 = {
+    'env' : 'Local',
+    'document' : './svfs/test3/{3D} 2431/{3D}.svf'
+};
+var options2 = {
+    'env' : 'Local',
+    'document' : './svfs/test1/{3D} 3969217/{3D}.svf'
+};
+var options3 = {
+  'env' : 'Local',
+  'document' : './svfs/test3/{3D} 2431/{3D}.svf'
+};
 
-      var options1 = {
-          'env' : 'Local',
-          'document' : './svfs/test1/Resource/3D вид/{3D} 6301435/{3D}.svf'
-      };
-      var options2 = {
-          'env' : 'Local',
-          'document' : './svfs/test2/Resource/3D вид/3D/3D.svf'
-      };
-      //svfs/test2/Resource/3D вид/3D/3D.svf
     
+Autodesk.Viewing.Initializer(options, function() {
+  viewer.start(options.document, options);
+});
       Autodesk.Viewing.Initializer(options1, function() {
-        viewer.start(options1.document, options1);
+        viewer1.start(options1.document, options1);
       });
       Autodesk.Viewing.Initializer(options2, function() {
-        viewer1.start(options2.document, options2);
-      });
-      Autodesk.Viewing.Initializer(options1, function() {
-        viewer2.start(options1.document, options1);
+        viewer2.start(options2.document, options2);
       });
       Autodesk.Viewing.Initializer(options, function() {
         viewer3.start(options.document, options);
       });
 
+      // height: 680px;
+      // width: 1000px;
 
-      let win = myViewerDiv.querySelector('.adsk-viewing-viewer');
-      win.style= "height: 540px; width: 640px; overflow: hidden;";
+let win = myViewerDiv.querySelector('.adsk-viewing-viewer');
+win.style= "height: 540px; width: 640px; overflow: hidden;";
 
       let win1 = myViewerDiv1.querySelector('.adsk-viewing-viewer');
       win1.style= "height: 540px; width: 640px; overflow: hidden;";
