b8427e00b587b759837871cea1c2af644704365f
Bogdan <b@bmazur.ru>
2021-11-26 13:27:18 +0300

    #Update++

--- pages/viewer.js
+++ pages/viewer.js
@@ -22,35 +22,31 @@ var options2 = {
 };
 var options3 = {
   'env' : 'Local',
-  'document' : './svfs/test3/{3D} 2431/{3D}.svf'
+  'document' : './svfs/test4/{3D} 6742053/{3D}.svf'
 };
 
-    
 Autodesk.Viewing.Initializer(options, function() {
   viewer.start(options.document, options);
 });
-      Autodesk.Viewing.Initializer(options1, function() {
-        viewer1.start(options1.document, options1);
-      });
-      Autodesk.Viewing.Initializer(options2, function() {
-        viewer2.start(options2.document, options2);
-      });
-      Autodesk.Viewing.Initializer(options, function() {
-        viewer3.start(options.document, options);
-      });
-
-      // height: 680px;
-      // width: 1000px;
+Autodesk.Viewing.Initializer(options1, function() {
+  viewer1.start(options1.document, options1);
+});
+Autodesk.Viewing.Initializer(options2, function() {
+  viewer2.start(options2.document, options2);
+});
+Autodesk.Viewing.Initializer(options3, function() {
+  viewer3.start(options3.document, options3);
+});
 
 let win = myViewerDiv.querySelector('.adsk-viewing-viewer');
 win.style= "height: 540px; width: 640px; overflow: hidden;";
 
-      let win1 = myViewerDiv1.querySelector('.adsk-viewing-viewer');
-      win1.style= "height: 540px; width: 640px; overflow: hidden;";
+let win1 = myViewerDiv1.querySelector('.adsk-viewing-viewer');
+win1.style= "height: 540px; width: 640px; overflow: hidden;";
 
-      let win2 = myViewerDiv2.querySelector('.adsk-viewing-viewer');
-      win2.style= "height: 540px; width: 640px; overflow: hidden;";
+let win2 = myViewerDiv2.querySelector('.adsk-viewing-viewer');
+win2.style= "height: 540px; width: 640px; overflow: hidden;";
 
-      let win3 = myViewerDiv3.querySelector('.adsk-viewing-viewer');
-      win3.style= "height: 540px; width: 640px; overflow: hidden;";
+let win3 = myViewerDiv3.querySelector('.adsk-viewing-viewer');
+win3.style= "height: 540px; width: 640px; overflow: hidden;";
       
